2540a4b41d530af12bae1580910e1881422c64ba
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-03-08 15:08:27 +0530

    [sanjana] Add : Fibonacci Series

---
+++ LogicalDay6/FibonacciSeries.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalDay6
+{
+    public class FibonacciSeries
+    {
+        public void CreateSeries(int n)
+        {
+            int a = -1, b = 1, c = 0;
+            for(int  i = 0; i < n;i++)
+            {
+                c = a + b;
+                Console.Write(c+" ");
+                a = b;
+                b = c;
+            }
+            Console.WriteLine("");
+        }
+    }
+}
--- LogicalDay6/Program.cs
+++ LogicalDay6/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome To Logical Programming");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalDay6
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logical Programming");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Select an option 1.Fibonacci Series 2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the length of the series");
+                        int n = Convert.ToInt32(Console.ReadLine());
+                        FibonacciSeries series = new FibonacciSeries();
+                        series.CreateSeries(n);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
