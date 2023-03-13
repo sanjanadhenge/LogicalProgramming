2dfa20d2ce2374b40d91086508955c7a62c08081
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-03-13 16:24:21 +0530

    [sanjana] Add :StopWatch using DateTime

--- LogicalDay6/StopWatch.cs
+++ LogicalDay6/StopWatch.cs
@@ -13,21 +13,22 @@ namespace LogicalDay6
             const string start = "Start", Stop = "Stop";
             Console.WriteLine("Enter Start to start the watch");
             string START = Console.ReadLine();
-            TimeSpan timeSpan = TimeSpan.Zero;
+            DateTime timeSpan = DateTime.Now;
             if (start.Equals(START))
                 {
-                     int Hour1 = timeSpan.Hours;
-                     int Min1 = timeSpan.Minutes;
-                     int Sec1 = timeSpan.Seconds;
-                     int Millisec1 = timeSpan.Milliseconds;
+                     int Hour1 = timeSpan.Hour;
+                     int Min1 = timeSpan.Minute;
+                     int Sec1 = timeSpan.Second;
+                     int Millisec1 = timeSpan.Millisecond;
                 Console.WriteLine("Enter Stop to Stop the watch");
                 string STOP = Console.ReadLine();
                 if (Stop.Equals(STOP))
                 {
-                    int Hour2 = timeSpan.Hours;
-                    int Min2 = timeSpan.Minutes;
-                    int Sec2 = timeSpan.Seconds;
-                    int Millisec2 = timeSpan.Milliseconds;
+                    DateTime timeSpan1 = DateTime.Now;
+                    int Hour2 = timeSpan1.Hour;
+                    int Min2 = timeSpan1.Minute;
+                    int Sec2 = timeSpan1.Second;
+                    int Millisec2 = timeSpan1.Millisecond;
                     Console.WriteLine("StopWatch Time" + (Hour2 - Hour1) +" "+ (Min2 - Min1) +" "+ (Sec2 - Sec1) +" "+ (Millisec2 - Millisec1)); 
                 }
 
